49588c4755db9cb1b8ed9ca2e526ebdc945bbc13
Shashwot <shashwot@DESKTOP-JTVR00V.localdomain>
2021-04-11 21:32:16 +0545

    homework

--- homework.py
+++ homework.py
@@ -22,4 +22,35 @@ def two_of_three(x,y,z):
 print(two_of_three(3,2,1)) 
 print(two_of_three(3,4,5)) 
 print(two_of_three(8,10,6))        
+
+#question 4
+def largest_factor(x):
+    for i in range(1,x):
+        if(x%i==0):
+            k=i
+    return k        
+print(largest_factor(49))
+
+#question 5
+
+
+
+
+
+
+
+#question 6
+def hailstone(x):
+    print(x)
+    while(x!=1):
+        if(x%2==0):
+            x=x/2
+            print(x)
+        elif(x%2!=0):
+            x=3*x+1
+            print(x)
+hailstone(10)
+
+         
+
  
